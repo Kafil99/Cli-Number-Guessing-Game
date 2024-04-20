1773f79e13f00a604b6faf557cb828b581fbef42
Kafil99 <163079546+Kafil99@users.noreply.github.com>
2024-04-20 22:21:50 +0500

    simple-cli-guessing-game
    A simple cli based number guessing Game

---
+++ Cli-Number-Guessing/main.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ramdomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "userGussedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10",
+    }]);
+if (answer.userGussedNumber === ramdomNumber) {
+    console.log("Congratulations!!! you guessed right number. ");
+}
+else {
+    console.log("You guessed wrong number");
+}
